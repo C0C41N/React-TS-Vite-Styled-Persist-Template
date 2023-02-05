0088b048e727af02d15cf2fbed2137f65fdcda11
C0C41N <0ct41n@gmail.com>
2023-02-05 14:31:36 +0500

    Basic Setup Done

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux'
+
+import Button from '@components/Button'
+import { setDummy } from '@mainSlice'
+import { useMainSlice } from '@mainSlice/hooks'
+import { T48Bold } from '@styles/typo'
+
+import { Container } from './styled'
+
+const Home = () => {
+	const dispatch = useDispatch()
+
+	const dummy = useMainSlice(e => e.dummy)
+
+	const updateDummy = (e: string) => dispatch(setDummy(e))
+
+	const onClick = () => {
+		const time = new Date().getTime()
+		updateDummy(time.toString())
+	}
+
+	return (
+		<Container>
+			<T48Bold>{dummy}</T48Bold>
+			<Button above={50} onClick={onClick}>
+				Click here
+			</Button>
+		</Container>
+	)
+}
+
+export default Home
